c7d9b358fdb7740f80f10d853275573638c72236
Alexander Salas <a.salas@ieee.org>
2015-01-23 10:15:06 -0430

    Addon Template

---
+++ bootstrap.js
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+let {interfaces: Ci, utils: Cu, classes: Cc} = Components;
+
+function startup(aData, aReason) {
+}
+
+function shutdown(aData, aReason) {
+}
+
+function install(aData, aReason) {
+}
+
+function uninstall(aData, aReason) {
+}
